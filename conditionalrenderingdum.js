aed02629c84ae77e585b06c3d12d42d1e5a9a33a
lallyk <114976253+lallyk@users.noreply.github.com>
2022-11-07 22:26:18 +0530

    task12

---
+++ conditionalrenderingdum.js
@@ -0,0 +1,239 @@
+//ExpenseDate.js
+
+import React from "react";
+
+import "./ExpenseDate.css";
+
+const ExpenseDate = (props) => {
+  const month = props.date.toLocaleString("en-US", { month: "long" });
+  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const year = props.date.getFullYear();
+
+  return (
+    <div className="expense-date">
+      <div className="expense-date__month">{month}</div>
+      <div className="expense-date__year">{year}</div>
+      <div className="expense-date__day">{day}</div>
+    </div>
+  );
+};
+
+export default ExpenseDate;
+
+//App.js
+
+import React, { useState } from "react";
+
+import NewExpense from "./components/NewExpenses/NewExpense";
+import Expenses from "./components/Expenses";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
+
+const App = () => {
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses];
+    });
+  };
+
+  // return React.createElement(
+  //   'div',
+  //   {},
+  //   React.createElement('h2', {}, "Let's get started!"),
+  //   React.createElement(Expenses, { items: expenses })
+  // );
+
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
+
+//ExpenseItem.js
+
+import React from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  return (
+    <li>
+      <Card className="expense-item">
+        <ExpenseDate date={props.date} />
+        <div className="expense-item__description">
+          <h2>{props.title}</h2>
+          <div className="expense-item__price">${props.amount}</div>
+        </div>
+      </Card>
+    </li>
+  );
+};
+
+export default ExpenseItem;
+
+//ExpenseList.js
+
+import React from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import "./ExpensesList.css";
+
+const ExpensesList = (props) => {
+  if (props.items.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expenses.</h2>;
+  }
+
+  return (
+    <ul className="expenses-list">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpensesList;
+
+//ExpenseForm.js
+
+import "./ExpenseForm.css";
+import React, { useState } from "react";
+const ExpenseForm = (props) => {
+  const [enteredTitle, setenteredTitle] = useState("");
+  const [enteredAmount, setenteredAmount] = useState("");
+  const [enteredDate, setenteredDate] = useState("");
+  const titleChangeHandler = (event) => {
+    setenteredTitle(event.target.value);
+  };
+  const amountChangeHandler = (event) => {
+    setenteredAmount(event.target.value);
+  };
+  const dateChangeHandler = (event) => {
+    setenteredDate(event.target.value);
+  };
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    //console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
+    setenteredTitle("");
+    setenteredAmount("");
+    setenteredDate("");
+  };
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__controls">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__controls">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredAmount}
+            onChange={amountChangeHandler}
+          />
+        </div>
+        <div className="new-expense__controls">
+          <label>Data</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            step="2022-12-31"
+            value="enteredDate"
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <label>Data</label>
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
+export default ExpenseForm;
+
+//Expenses.js
+
+import React, { useState } from "react";
+
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
+
