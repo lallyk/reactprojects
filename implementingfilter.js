c877c06e59d7a99de4abfbff1df92e1f7fa25578
lallyk <114976253+lallyk@users.noreply.github.com>
2022-11-07 21:57:13 +0530

    task10

---
+++ implementingfilter.js
@@ -0,0 +1,148 @@
+//Expenses.js
+import React, { useState } from "react";
+import ExpenseItem from "./ExpenseItem";
+import Card from "./Card";
+import ExpensesFilter from "./ExpensesFilter";
+import "./Expenses.css";
+
+const Expenses = (props) => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {props.items.map((expense) => (
+          <ExpenseItem
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
+    </div>
+  );
+};
+
+export default Expenses;
+//app.js
+import NewExpense from "./components/NewExpenses/NewExpense";
+//import ExpensesFilter from "./components/ExpensesFilter";
+//import React, { useState } from "react";
+import Expenses from "./components/Expenses";
+const App = () => {
+  const expenses = [
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
+  ];
+
+  const addExpenseHandler = (expense) => {
+    console.log("In App.js");
+    console.log(expense);
+  };
+  return (
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses items={expenses} />
+    </div>
+  );
+};
+
+export default App;
+
+
+//ExpenseFilter.js
+
+import React from "react";
+import "./ExpensesFilter.css";
+const ExpensesFilter = (props) => {
+  const dropDownChangeHandler = (event) => {
+    props.onChangeFilter(event.target.value);
+  };
+  return (
+    <div className="expense-filter">
+      <div className="expenses-filter__control">
+        <label>Filter by Year</label>
+        <select value={props.selected} onChange={dropDownChangeHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+          <option value="2018">2018</option>
+          <option value="2017">2017</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+export default ExpensesFilter;
+
+//ExpenseItem.js
+
+import React from "react";
+
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
+
+const ExpenseItem = (props) => {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
+};
+
+export default ExpenseItem;
+
+//NewExpense.js
+
+import React from "react";
+import "./NewExpense.css";
+import ExpenseForm from "./ExpenseForm";
+const NewExpense = (props) => {
+  const saveExpenseDataHandler = (enteredExpenseData) => {
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString(),
+    };
+    //console.log(expenseData);
+    props.onAddExpense(expenseData);
+  };
+  return (
+    <div className="new-expense ">
+      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+    </div>
+  );
+};
+export default NewExpense;
+
+
